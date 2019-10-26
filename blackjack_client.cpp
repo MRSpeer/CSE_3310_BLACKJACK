b2ee7c5d29ef7ca7b677619476956b29f2e3f694
LeutrimD1 <45049970+LeutrimD1@users.noreply.github.com>
2019-10-26 13:21:52 -0500

    Update blackjack_client.cpp
    blank window

--- blackjack_client.cpp
+++ blackjack_client.cpp
@@ -1,2 +1,16 @@
 #include <gtkmm.h>
 #include <gtkmm/application.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  auto app =
+    Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_default_size(200, 200);
+
+  return app->run(window);
+}
