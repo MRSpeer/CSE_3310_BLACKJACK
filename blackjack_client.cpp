ef0806feed0928caeed5819e36533f15e9b34a74
leutrim_pi <leutrimd1@gmail.com>
2019-10-28 15:24:58 -0500

    edited the main window for BJP

--- blackjack_client.cpp
+++ blackjack_client.cpp
@@ -3,14 +3,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+using namespace Gtk;
+using namespace Glib;
 int main(int argc, char *argv[])
 {
   auto app =
     Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
   Gtk::Window window;
-  window.set_default_size(200, 200);
-
+  window.set_default_size(900, 900);
+  window.set_position(WIN_POS_CENTER);
+  window.set_title("BlackJack Player");
   return app->run(window);
 }
